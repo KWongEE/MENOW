7ab803d46e7a8625db239978a6bfd702a1eaec12
Kevin <kwongca.95@gmail.com>
2018-05-11 01:22:32 -0400

    Need to update tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_photo])
-  # end
-
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password,
     :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar) }
