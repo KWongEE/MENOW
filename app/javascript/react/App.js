569dd0aef81ad24100301bf5e461bb1c3ba55a96
Kevin <kwongca.95@gmail.com>
2018-05-11 12:55:18 -0400

    Merge master to google_maps

--- app/javascript/react/App.js
+++ app/javascript/react/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute, Router, browserHistory } from 'react-router';
-<<<<<<< HEAD
-// import CatsIndexContainer from './containers/CatsIndexContainer'
-import Map from './components/Map'
-=======
 import CatsIndexContainer from './containers/CatsIndexContainer'
 import CatFormContainer from './containers/CatFormContainer'
 import CatShowContainer from './containers/CatShowContainer'
->>>>>>> master
 
 
 
@@ -15,17 +10,11 @@ const App = (props) => {
   return(
     <Router history={browserHistory}>
       <Route path='/'>
-<<<<<<< HEAD
-        <IndexRoute component={Map} />
-
-          </Route>
-=======
         <IndexRoute component={CatsIndexContainer} />
         <Route path='/cats/new' component={CatFormContainer} />
         <Route path="/cats/:id" component={CatShowContainer}/>
         <Route path="*" component={CatsIndexContainer} />
       </Route>
->>>>>>> master
     </Router>
   )
 }
