68ef81e4c4d4aaead6c83e02114bef1172a67874
Kevin <kwongca.95@gmail.com>
2018-05-11 11:33:43 -0400

    Fixed rspec

--- spec/controllers/api/v1/cats_controller_spec.rb
+++ spec/controllers/api/v1/cats_controller_spec.rb
@@ -51,32 +51,4 @@ RSpec.describe Api::V1::CatsController, type: :controller do
       expect(returned_json).to_not be_kind_of(Array)
     end
   end
-#Moved to CatController
-  # describe "POST#create" do
-  #   before(:each) do
-  #     DatabaseCleaner.clean_with :truncation
-  #     @user = User.create(email: "testing@gmail.com", password: "password")
-  #     sign_in(@user)
-  #     @cat = Cat.create( name: "Joshua", description: "Long maned cat", location: "77 Street")
-  #   end
-  #
-  #   it "should have a current_user" do
-  #     session[:user_id] = @user.id
-  #     expect(controller.current_user).to eq(@user)
-  #   end
-  #
-  #   it "should create a new cat" do
-  #     post(:create, params: {cats: {name: "Joshua",description:"Long maned cat", location: "77 Street"}} )
-  #     returned_json = JSON.parse(response.body)
-  #     expect(response.status).to eq 200
-  #     expect(response.content_type).to eq "application/json"
-  #
-  #     expect(returned_json.length).to eq 1
-  #     expect(returned_json.last["name"]).to eq @cat.name
-  #     expect(returned_json.last["description"]).to eq @cat.description
-  #     expect(returned_json.last["location"]).to eq @cat.location
-  #     expect(returned_json.last["spotter"]). to eq @user.email
-  #     expect(returned_json.last["user_id"]). to eq @user.id
-  #   end
-  # end
 end
