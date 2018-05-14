70e81b0024c997a0cd9dd79c3f7c192a2fe8d80e
Kevin <kwongca.95@gmail.com>
2018-05-14 11:46:04 -0400

    Fixed PR

--- app/javascript/react/components/CatShow.js
+++ app/javascript/react/components/CatShow.js
@@ -3,15 +3,12 @@ import React from 'react'
 const CatShow = (props) => {
   return(
     <div>
-    <div className="box"></div>
-
     <div className="catShow">
       <div>Cat Name ฅ(*ΦωΦ*)ฅ: {props.name}</div><br></br>
       <div>Cat Description: {props.description}</div><br></br>
       <div>Last seen: {props.location}</div><br></br>
       <div>Who found it?: {props.spotter}</div><br></br>
       <img src={props.image.url} />
-    </div>
   </div>
   )
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # attr_accessor :login
-
   def login=(login)
     @login = login
   end
@@ -10,20 +6,11 @@ class User < ApplicationRecord
   def login
     @login || self.username || self.email
   end
-  
+
   has_many :cats
 
   mount_uploader :avatar, ProfilePhotoUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-#   def self.find_first_by_auth_conditions(warden_conditions)
-#   conditions = warden_conditions.dup
-#   if login = conditions.delete(:login)
-#     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-#   else
-#     where(conditions).first
-#   end
-# end
 end
