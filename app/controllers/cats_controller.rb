7a8f3f6e165a914f416b4903ad6e1e77e03766b6
Kevin <kwongca.95@gmail.com>
2018-05-30 22:49:39 -0400

    WIP create method. Getting response error

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class CatsController < ApplicationController
   def new
     @cat = Cat.new
@@ -5,12 +6,14 @@ class CatsController < ApplicationController
 
   def create
     @cat = Cat.new(cat_params)
+    binding.pry
     if current_user
       @cat.user = current_user
     else
       @cat.user_id = 1
     end
     if @cat.save
+      binding.pry
       redirect_to '/', notice: "#{@cat.name} has been uploaded."
     else
       render "new"
@@ -20,7 +23,7 @@ class CatsController < ApplicationController
   def edit
     @cat = Cat.find(params[:id])
   end
-  
+
   def update
     @cat = Cat.find(params[:id])
     if @cat.update_attributes(cat_params)
---
+++ spec/controllers/cats_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe CatsController, type: :controller do
+  # before(:each) do
+  #   user1 = User.create(email: "test@test.com", password: "password")
+  #   binding.pry
+  #   cat1 = Cat.create(name: "Joshua", description: "Long maned cat", location: "77 Street", user: user1)
+  #   binding.pry
+  #   cat2 = Cat.create(name: "Garfield's Brother", description: "Super Skinny", location: "Lasagna Street", user: user1)
+  # end
+
+  describe "POST#create" do
+   before(:each) do
+      # DatabaseCleaner.clean_with :truncation
+      @user = User.create(email: "testing@gmail.com", password: "password")
+      sign_in(@user)
+      @cat = Cat.create(name: "Joshua", description: "Long maned cat", location: "77 Street", user: @user)
+    end
+
+    it "should have a current_user" do
+      sign_in(@user)
+      session[:user_id] = @user.id
+      expect(controller.current_user).to eq(@user)
+    end
+
+    it "should create a new cat" do
+      # post(:create, params: {cats: {name: "Joshua",description:"Long maned cat", location: "77 Street"}} )
+      post :create, params: {cat: {name:"Joshua",description:"Long maned cat",location:"77 Street", user: @user}}, format: :json
+binding.pry
+      returned_json = JSON.parse(response.body)
+      binding.pry
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq "application/json"
+
+      expect(returned_json.length).to eq 1
+      expect(returned_json.last["name"]).to eq @cat.name
+      expect(returned_json.last["description"]).to eq @cat.description
+      expect(returned_json.last["location"]).to eq @cat.location
+      expect(returned_json.last["spotter"]). to eq @user.email
+      expect(returned_json.last["user_id"]). to eq @user.id
+    end
+  end
+end
